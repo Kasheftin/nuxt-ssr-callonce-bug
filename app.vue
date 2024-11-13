0b951710d49167d664acc20379fdbfaa741cd930
Kasheftin <Kasheftin@gmail.com>
2024-11-13 11:00:25 +0200

    initial commit - the basic hello-world with callOnce does not do SSR

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    ddd: {{ ddd }}
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const ddd = useState('ddd')
+
+callOnce(async () => {
+  const data = await $fetch('https://new.lozinskis.com/items/pages')
+  ddd.value = data
+  console.log('callOnce', data)
+})
+</script>
